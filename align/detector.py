4cf3a956ed7e50f75dd4b33def7f2e382f36ad96
Ken Oung <e0003273@u.nus.edu>
2019-06-06 17:35:23 +0800

    Update detector.py

--- align/detector.py
+++ align/detector.py
@@ -76,7 +76,7 @@ def detect_faces(image, min_face_size = 20.0,
     # STAGE 2
 
     img_boxes = get_image_boxes(bounding_boxes, image, size = 24)
-    img_boxes = torch.FloatTensor(img_boxes, requires_grad=False)
+    img_boxes = torch.tensor(img_boxes, requires_grad=False)
     output = rnet(img_boxes)
     offsets = output[0].data.numpy()  # shape [n_boxes, 4]
     probs = output[1].data.numpy()  # shape [n_boxes, 2]
@@ -97,7 +97,7 @@ def detect_faces(image, min_face_size = 20.0,
     img_boxes = get_image_boxes(bounding_boxes, image, size = 48)
     if len(img_boxes) == 0: 
         return [], []
-    img_boxes = torch.FloatTensor(img_boxes, requires_grad=False)
+    img_boxes = torch.tensor(img_boxes, requires_grad=False)
     output = onet(img_boxes)
     landmarks = output[0].data.numpy()  # shape [n_boxes, 10]
     offsets = output[1].data.numpy()  # shape [n_boxes, 4]
